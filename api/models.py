e01b192bb1cf69561536c389ff180dfde12b1de5
Edgar Mendes <mendesbr92@gmail.com>
2024-05-31 00:10:40 -0300

    feat: employee models created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
+class Employee (models.Model):
+
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    lastname = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    cpf = models.CharField(
+        max_length=14,
+        null=False,
+        blank=False
+    )
+
+    service_time = models.IntegerField(
+        default=0,
+        null=False,
+        blank=False
+    )
+
+    remuneration = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        null=False,
+        blank=False
+    )
+
+    object = models.Manager()
+
